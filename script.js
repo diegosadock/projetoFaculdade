1de032045b269adda305a10189cf8e32cfefb5ec
Diego Sadock <95287173+diegosadock@users.noreply.github.com>
2024-06-04 10:57:31 -0300

    Add files via upload
    Second commit

--- script.js
+++ script.js
@@ -10,5 +10,16 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000); 
+}
+
+function plusSlides(n) {
+  slideIndex += n;
+  const slides = document.getElementsByClassName("mySlides");
+  if (slideIndex > slides.length) { slideIndex = 1; }
+  if (slideIndex < 1) { slideIndex = slides.length; }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
 }
